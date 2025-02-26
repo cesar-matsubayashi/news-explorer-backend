9b4c3816ea5cb46cdcde3d972c4ff9f4aebda8d9
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2025-02-26 14:12:20 -0300

    feat: add article schema

---
+++ models/article.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  fonte: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+      message: "O link informado não é válido",
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+      message: "O link informado não é válido",
+    },
+  },
+  owner: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    select: false,
+  },
+});
+
+module.exports = mongoose.model("article", articleSchema);
