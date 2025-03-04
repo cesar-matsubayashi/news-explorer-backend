da7407e7da77a04afd9116850dbcec9d2d522249
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2025-03-03 21:43:35 -0300

    feat: add form-data input format

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
+const multer = require("multer");
+const upload = multer();
 const { celebrate, Joi } = require("celebrate");
 const { createUser, login } = require("../controllers/user");
 
 router.post(
   "/signup",
+  upload.none(),
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
