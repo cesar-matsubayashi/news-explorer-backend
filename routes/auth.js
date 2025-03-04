f47e4a3cf633e37872c241696470dbe2333956d8
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2025-03-04 00:14:52 -0300

    fix: add login formdata input format

--- routes/auth.js
+++ routes/auth.js
@@ -19,10 +19,11 @@ router.post(
 
 router.post(
   "/signin",
+  upload.none(),
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
+      password: Joi.string().required(),
     }),
   }),
   login
